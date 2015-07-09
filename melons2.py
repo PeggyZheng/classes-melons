5a45ace301ff9af392c1fd8cefe6e6e8e723f673
Hackbright Student <info@hackbrightacademy.com>
2015-07-08 17:56:31 -0700

    try except added

--- melons2.py
+++ melons2.py
@@ -1,35 +1,38 @@
 """This file should have our melon-type classes in it."""
 
 class Melons(object):
-    # def __init__(self,:
-    #     self.species = species
-    #     self.color = color
-    #     self.shape = shape
-    #     self.origin = origin
-    #     self.seasons = seasons
 
     def get_base_price(self):
         """Calculate price, given a number of melons ordered."""
+        try:   
+            self.base_price = 5  
+            if self.species == "Casabas" or self.species == "Ogens":
+                self.base_price += 1
+            if self.origin == True:
+                self.base_price *= 1.5
+            if self.shape == "square":
+                self.base_price *= 2
+            return self.base_price
 
-        self.base_price = 5  
-        if self.species == "Casabas" or self.species == "Ogens":
-            self.base_price += 1
-        if self.origin == True:
-            self.base_price *= 1.5
-        if self.shape == "square":
-            self.base_price *= 2
-        return self.base_price
+        except AttributeError
+            print "These are not melons!"
 
     def get_total_price(self, qty):
-        if self.species == "Watermelon" and qty >= 3:
-            return self.get_base_price() * qty * 0.75
-        elif self.species == "Cantaloupe" and qty >= 5:
-            return self.get_base_price() * qty * 0.5
-        else:
-            return self.get_base_price() * qty
+        
+        try:
+            if self.species == "Watermelon" and qty >= 3:
+                return self.get_base_price() * qty * 0.75
+            elif self.species == "Cantaloupe" and qty >= 5:
+                return self.get_base_price() * qty * 0.5
+            else:
+                return self.get_base_price() * qty
+
+        except AttributeError:
+            print "These are not melons!"
 
 #         return total
-
+m = Melons()
+print m.get_total_price(2)
 
 class Watermelon(Melons):
     species = "Watermelon"
@@ -64,8 +67,8 @@ class Sharlyn(Melons):
 
     
         
-class Santa_Claus(Melons):
-    species = "Santa_Claus"
+class SantaClaus(Melons):
+    species = "SantaClaus"
     color = "green"
     origin = True
     shape = 'natural'
@@ -82,8 +85,8 @@ class Christmas(Melons):
 
     
 
-class Horned_Melon(Melons):
-    species =  "Horned_Melon"
+class HornedMelon(Melons):
+    species =  "HornedMelon"
     color = "yellow"
     origin = True
     shape = 'natural'
